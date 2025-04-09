532696f182c76defeea787ed21ba2956798039a0
Maksym.Zyza <mmziza88@gmail.com>
2025-04-09 13:27:47 +0300

    fix(deque): remove spaces

--- Deque.py
+++ Deque.py
@@ -1,15 +1,15 @@
 from collections import deque
 
 def check_palindrome(word):
-    d = deque()
-    for letter in word:
-        d.append(letter.lower())  
-
+    cleaned = ''.join(word.lower().split()) 
+    d = deque(cleaned)
+    
     while len(d) > 1:
         if d.popleft() != d.pop():
             return False
     return True
 
 # Test 
-print('Madam:', check_palindrome("Madam"))
-print('Python:', check_palindrome("Python"))    
+print('Madam:', check_palindrome("Madam"))           
+print('Python:', check_palindrome("Python"))         
+print('A Santa at NASA:', check_palindrome("A Santa at NASA")) 
